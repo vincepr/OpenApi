9cc5ef43b1480b86921726caf27a6915f3ba15bd
vince <vincepr@gmx.de>
2025-01-14 23:57:05 +0100

    just enough styling to use it

--- BlazorPage/Program.cs
+++ BlazorPage/Program.cs
@@ -1,14 +1,6 @@
-using System.Diagnostics;
-using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorPage;
-using Microsoft.OpenApi;
-using Microsoft.OpenApi.Any;
-using Microsoft.OpenApi.Extensions;
-using Microsoft.OpenApi.Models;
-using Microsoft.OpenApi.Readers;
-using Microsoft.OpenApi.Writers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
